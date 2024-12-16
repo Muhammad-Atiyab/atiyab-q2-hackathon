8ec9858d09503edd7a3a4820ffdfcec6131282d8
Muhammad-Atiyab <160265942+Muhammad-Atiyab@users.noreply.github.com>
2024-12-16 22:38:56 +0500

    Updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${josefinSans.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${josefinSans.variable} ${lato.variable} antialiased`}
       >
         <Header/>
         {children}
